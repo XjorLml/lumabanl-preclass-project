6134647a6fcfed916c216acc4eb072aa6b9783d6
Xjor.lml <lemnuellumaban@gmail.com>
2023-02-22 16:47:44 +0800

    activity_2: create scrip to concatenate fullname from hash

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,14 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def full_name(name)
+
+  name.each do|key, value|
+    puts "fname: #{key}, lname: #{value}"
+    full = value + value
+    return full
+  end
+end
+
+p full_name({fname: 'Lemnuel',  mname: 'Saway', lname: 'Lumaban'})

bf44f271e73818fe95cc596c35c4caa4698c6722
Xjor.lml <lemnuellumaban@gmail.com>
2023-02-22 15:35:21 +0800

    get average of adults ages

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,22 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+#
+
+def average_age(ages)
+  adults = []
+  total = 0
+
+  ages.each do|age|
+    if age >= 18
+      adults << age
+      total = total + age
+    end
+  end
+  return total/ adults.size
+
+
+
+end
+
+p average_age([12, 17, 18, 23, 25, 30])

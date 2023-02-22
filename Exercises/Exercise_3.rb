21dc622e026d48c0dd3d67c42702203818ba2bd0
Xjor.lml <lemnuellumaban@gmail.com>
2023-02-22 14:32:14 +0800

    Exercise3: create scrip to separate adults using array

---
+++ Exercises/Exercise_3.rb
@@ -0,0 +1,13 @@
+def who_is_adult(ages)
+  adults = []
+  
+  ages.each do|age|
+    if age >= 18
+      adults << age
+    end
+  end
+
+  return adults
+end
+
+p who_is_adult([10, 20, 17, 18, 22, 30])
